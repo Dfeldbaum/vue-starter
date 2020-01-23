f011512d559e8e15ba47e9789cf2f2824c23259a
David Feldbaum <david.feldbaum@icfolson.com>
2020-01-23 13:49:05 -0600

    added test component

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">test</div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .test {
+    color: red;
+    background: blue;
+  }
+</style>
